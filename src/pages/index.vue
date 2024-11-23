b2f5c334283cb0cf5751e47362adec2c35cc2c24
xiemenwaidao <menxielijiao@gmail.com>
2024-11-24 01:19:28 +0900

    ➕ tailwind/shadcnの追加、src構成

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+</script>
+
+<template>
+  <Card class="w-[350px]">
+    <CardHeader>
+      <CardTitle>Create project</CardTitle>
+      <CardDescription>Deploy your new project in one-click.</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <!--  -->
+    </CardContent>
+    <CardFooter class="flex justify-between px-6 pb-6">
+      <!--  -->
+    </CardFooter>
+  </Card>
+</template>
